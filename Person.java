858dc22887ab5d5957b1caef34f16610f9da0e32
Fadejimi Banjoko <fadejimibanjoko@Fadejimis-MacBook-Air.local>
2025-08-28 19:04:00 -0400

    first commit

---
+++ Person.java
@@ -0,0 +1,15 @@
+class Person {
+    String name;
+    int age;
+    String eyeColor;
+
+    public Person() {
+       this.name = "Alex White";
+       this.age = 23;
+       this.eyeColor = "Red"; 
+    }
+
+    public Person(String name, int age, String eyeColor) {
+        this.name = name;
+    }
+}
